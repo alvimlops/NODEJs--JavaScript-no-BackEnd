93a9f7d4b3235fdee9b05caed87c6a8178f71e15
alvaro Rodrigues <alvim.lopez@gmail.com>
2022-01-21 13:52:44 -0300

    detectando URL diferente

--- restful/index.js
+++ restful/index.js
@@ -4,7 +4,25 @@ let server = http.createServer((req, res) =>{
  console.log('URL:', req.url);
  console.log('METHOD:', req.method);
 
- res.end('OK');
+   switch (req.url){
+       case '/':
+
+       res.statusCode = 200;
+       res.setHeader('content-Type', 'text/html');
+       res.end('<h1>ola</h1>');
+
+       break;
+
+       res.statusCode = 200;
+       res.setHeader('content-Type', 'application/json');
+       res.end(JSON.stringify({
+           users:[{
+               name:'Alvim',
+               email: 'alvim.lopez@gmail.com',
+               id:1
+           }]
+        }));
+   }
 });
 
 server.listen(3001, '127.0.0.1', ()=>{
